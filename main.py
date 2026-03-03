dbcb5b37d081d91e132b1d5e58d150be946070cf
Robin Bette <robinbette@hotmail.com>
2026-03-03 11:00:35 +0100

    Create a simple model

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class Item(BaseModel):
+    name: str
+    price: float
+    count: int
+    id: int
+
+
+items = {
+    0: Item(name="Hammer", price=9.99, count=20, id=0),
+    1: Item(name="Pliers", price=5.99, count=20, id=1),
+    2: Item(name="Nails", price=1.99, count=100, id=2),
+}
+
+
 @app.get('/')
-def index() -> str:
-    return 'Hello World!'
+def index() -> dict[str, dict[int, Item]]:
+    return {"items": items}
+
